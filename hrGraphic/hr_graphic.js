56f05957ec04219df73bbdcf46ef21bc205e566a
SeanMcClure <sean.mcclure17@gmail.com>
2020-06-19 18:40:25 -0400

    update code clarity

--- hrGraphic/hr_graphic.js
+++ hrGraphic/hr_graphic.js
@@ -11,36 +11,24 @@ d3.csv("total_hr_data.csv")
                     return "translate(" + (2 * d.hc_x + 50) + "," + (2 * d.hc_y + 50) + ")";
                 });
 
-                // hrs.append("line")
-                //          .attr("class", "lne")
-                //          .attr("x1", 320)
-                //          .attr("y1", 470)
-                //          .attr("x2",function(d) {
-                //               return d.hc_x;})
-                //          .attr("y2",function(d) {
-                //               return d.hc_y;})
-                //          .style("stroke", "blue")
-                //          .style("stroke-width", 0.5);
-
-      hrs.append("circle") // add a circle for each shot
+      hrs.append("circle") // add a circle for each hr
                .attr("r", 3)
                .attr("class", "cle")
                .attr("fill", function(d) {
                       return "blue";
               })
 
-
-      var batters = d3.nest() // filter by player and get total shots made by each player
+      var batters = d3.nest() // filter by player and get total hrs by each player
       .key(function(d){ return d.player_name; })
       .rollup(function(a){ return a.length; })
       .entries(d);
 
       console.log(batters);
 
-      batters.unshift({"key": "ALL", //create one entry that shows all shots
+      batters.unshift({"key": "ALL", 
                       "value": d3.sum(batters, function(d) { return d.value; })})
 
-     var selector = d3.select("#selector"); //make a selecter to choose desired player
+     var selector = d3.select("#selector"); 
 
      selector    //add player name and count to each item in the drop down
           .selectAll("option")
